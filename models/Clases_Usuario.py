5a0b2907177fb3831be2443e20ff7f6bbbe3d59d
Julean Pilligua <juleanpilligua2@gmail.com>
2026-05-26 21:47:45 -0500

    Definición de la clase Postulante

--- models/Clases_Usuario.py
+++ models/Clases_Usuario.py
@@ -62,3 +62,27 @@ class UsuarioAcademico(Usuario):
     def __init__(self, cedula, nombres, apellidos, correo, contrasena,):
         super().__init__(cedula, nombres, apellidos, correo, contrasena)
 
+class Postulante(UsuarioAcademico):
+    def __init__(self, cedula, nombres, apellidos, correo, contrasena, idPostulante, tipoMatricula, celular, jornada, modalidad, cupo, asistencia, sexo, etnia, discapacidad):
+        super().__init__(cedula, nombres, apellidos, correo, contrasena)
+        self._idPostulante = idPostulante
+        self._tipoMatricula = tipoMatricula
+        self._celular = celular
+        self.jornada = jornada
+        self.modalidad = modalidad
+        self.cupo = cupo
+        self.asistencia = asistencia
+        self.sexo = sexo
+        self.etnia = etnia
+        self.discapacidad = discapacidad
+        
+    def seleccionarJornada(self):
+        pass
+    def elegirModalidad(self):
+        pass
+    def matricularseEnMateria(self):
+        pass
+    def solicitarRetiro(self):
+        pass
+    def realizarPagoMatricula(self):
+        pass
