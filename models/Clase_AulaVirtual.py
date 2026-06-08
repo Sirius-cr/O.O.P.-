3e0ed7d8ca563375ddd4d26fccf7c150110631f1
KENNYA VELEZ <jostinvelez2005@gmail.com>
2026-06-07 19:28:02 -0500

    Esctructura del modulo academico clases

--- models/Clase_AulaVirtual.py
+++ models/Clase_AulaVirtual.py
@@ -1,6 +1,9 @@
-class AulaVirtual:
-    def __init__(self, identidicador_aula_virtual, url):
-        self.identificador_aula_virtual=identidicador_aula_virtual
-        self.url=url
-        
+from Clase_Entorno_Academico import EntornoAcademico
+class AulaVirtual(EntornoAcademico):
+    def __init__(self,identificadorEntorno,capacidadMaxima,tipoPlataforma):
+        super().__init__(identificadorEntorno,capacidadMaxima)
+        self.tipoPlataforma=tipoPlataforma
+    
+    def obtenerAcceso(self):
+        return print("Acceso a la plataforma:", self.tipoPlataforma)
 #composicion con <<interfaz>> con la clase de Materia
--- models/Clase_Horario.py
+++ models/Clase_Horario.py
@@ -1,7 +1,9 @@
 class Horario:
-    #agregacion con seccion
     def __init__(self, turno, hora_inicio, hora_fin, modalidad):
         self.turno = turno
         self.hora_inicio =hora_inicio
         self.hora_fin=hora_fin
         self.__modalidad=modalidad
+    
+    def ocultar_horario_lleno(self):
+        pass
