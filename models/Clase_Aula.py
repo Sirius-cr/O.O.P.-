23802671961c698ab2419aa58a046ac67617d898
Pilligua Saltos Michael Julean <juleanpilligua2@gmail.com>
2026-06-07 19:44:50 -0500

    Estructura del modulo academico clases

--- models/Clase_Aula.py
+++ models/Clase_Aula.py
@@ -1,8 +1,8 @@
 from Clase_Entorno_Academico import EntornoAcademico
 class Aula(EntornoAcademico):
     def __init__(self,identifiacadoEntorno,capacidadMaxima,ubicacionFisica):
-       super().__init__(identifiacadoEntorno,capacidadMaxima)
-       self.ubicacionFisica=ubicacionFisica
+        super().__init__(identifiacadoEntorno,capacidadMaxima)
+        self.ubicacionFisica=ubicacionFisica
 
     def obtenerAcceso(self):
         return print("La Aula fisica esta ubicada en ", self.ubicacionFisica)
