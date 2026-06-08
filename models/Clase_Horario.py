0ef622845f01e1dc2e17302c12df651501a967e3
KENNYA VELEZ <jostinvelez2005@gmail.com>
2026-06-08 09:38:52 -0500

    Cambios

--- models/Clase_Horario.py
+++ models/Clase_Horario.py
@@ -1,11 +1,21 @@
+from Clase_Seccion import Seccion
 class Horario:
     def __init__(self, turno, hora_inicio, hora_fin, modalidad):
         self.turno = turno
         self.hora_inicio =hora_inicio
         self.hora_fin=hora_fin
-        self.__modalidad=modalidad
+        self._modalidad=modalidad
     
     def Detenccion_Colision(self, otro_horario):
         if self.turno == otro_horario.turno:
             pass
         return False
+
+    def resumen_de_Seccion(self,Seccion:Seccion):
+        return {
+            "Turno de clase":self.turno,
+            "Inicializacion":self.hora_inicio,
+            "Terminacion" : self.hora_fin,
+            "Modalidad":self._modalidad,
+            "Docente":self.docente_asignado
+        }
