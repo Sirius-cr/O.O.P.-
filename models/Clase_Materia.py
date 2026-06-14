a41ab559d49737042e9453ee18cd55c4ba800924
Pilligua Saltos Michael Julean <juleanpilligua2@gmail.com>
2026-06-13 22:42:57 -0500

    Cambios del UML

--- models/Clase_Materia.py
+++ models/Clase_Materia.py
@@ -1,11 +1,12 @@
-from models.enums.Estado_Aprobacion import EstadoDeAprobacionMateria 
+from models.enums.Estado_Aprobacion import EstadoDeAprobacionMateria
 
 class Materia:
-    def __init__(self, id_materia, nombre_materia, nota_minima, asistencia_minima):
+    def __init__(self, id_materia, nombre_materia, nota_minima = EstadoDeAprobacionMateria.NOTA_MINIMA_APROBACION.value, asistencia_minima = EstadoDeAprobacionMateria.ASISTENCIA_MINIMA.value):
+        
         self.id_materia = id_materia
         self.nombre_materia = nombre_materia
-        self.nota_minima = EstadoDeAprobacionMateria.NOTA_MINIMA_APROBACION.value
-        self.asistencia_minima = EstadoDeAprobacionMateria.ASISTENCIA_MINIMA.value
+        self.nota_minima = nota_minima
+        self.asistencia_minima = asistencia_minima
         self.secciones=[]
     def crear_plan_estudio(self):
         return f"Felciidades! has creado un plan de estudios con exito"
